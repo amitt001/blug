6690d6c94dbc5f5d463cd54bd126dfa680544729
Amit Tripathi <mail2amit19@gmail.com>
2018-02-22 22:38:29 +0530

    Personal website setup

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
 from manage import app
+from views.home import home
 from views.blog import index, blog
 
+
+app.add_url_rule('/', view_func=home, methods=['GET'])
 app.add_url_rule('/blog', view_func=index, methods=['GET'])
 app.add_url_rule('/blog/<_id>/<blog_path>', view_func=blog, methods=['GET'])
